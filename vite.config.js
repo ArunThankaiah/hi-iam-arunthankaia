76f22cdc492cda50503f1736ddfcc445a29fff8d
ArunThankaiah <arunchandhu120@gmail.com>
2024-03-31 09:33:23 +0530

    first install

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"/hi-iam-arunthankaia/",
+  plugins: [react()],
+})
